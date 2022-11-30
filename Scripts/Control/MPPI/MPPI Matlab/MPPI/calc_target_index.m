8fefee5a302a884dfc1cbc0ffea5ab8f87bc43b5
LiAngHNU <104048584+LiAngHNU@users.noreply.github.com>
2022-11-30 16:01:58 +0800

    Scripts/Control/MPPI: Delete /MPPI Matlab

--- Scripts/Control/MPPI/MPPI Matlab/MPPI/calc_target_index.m
+++
@@ -1,6 +0,0 @@
-%% 寻找参考轨迹最近目标点
-function target_idx = calc_target_index(pos_x,pos_y, refPos_x,refPos_y)
-i = 1:length(refPos_x)-1;
-dist = sqrt((refPos_x(i)-pos_x).^2 + (refPos_y(i)-pos_y).^2);
-[~, target_idx] = min(dist);
-end
--- Scripts/Control/MPPI/MPPI Matlab/MPPI/clamp_u.m
+++
@@ -1,9 +0,0 @@
-function result = clamp_u(u, u_max, u_min)
-result = u;
-if result>u_max
-    result = u_max;
-end
-if result<u_min
-    result = u_min;
-end
-end
