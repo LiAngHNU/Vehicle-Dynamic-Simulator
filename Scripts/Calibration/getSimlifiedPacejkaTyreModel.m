28d4e34d444b99c7aa78a76270c778bec6089f25
Li.Ang <ang1997@hnu.edu.cn>
2022-11-30 16:25:50 +0800

    Scripts/Calibration: Update getSimplifiedPacejkaTyreModel.m

--- Scripts/Calibration/getSimlifiedPacejkaTyreModel.m
+++ Scripts/Calibration/getSimlifiedPacejkaTyreModel.m
@@ -9,13 +9,15 @@ close all; clear; clc;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Generator Settings %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Set Vehicle
+Vehicle = "Foton_Auman";
 % Load Vehicle
-load("Configs\Foton_Auman\Foton_Auman_Calib.mat");
+load(strcat("Configs\",Vehicle,"\",Vehicle,"_Calib.mat"));
 Fzf = 9.8*Vehicle_Calib.Mf;
 Fzr = 9.8*Vehicle_Calib.Mr;
 % Load Original Tyre Data
-load("Configs\Foton_Auman\Foton_Auman_TyreFx_Original.mat");
-load("Configs\Foton_Auman\Foton_Auman_TyreFy_Original.mat");
+load(strcat("Configs\",Vehicle,"\",Vehicle,"_TyreFx_Original.mat"));
+load(strcat("Configs\",Vehicle,"\",Vehicle,"_TyreFy_Original.mat"));
 len_Fx = 2*length(Tyre_Fx)-1;
 len_Fy = 2*length(Tyre_Fy)-1;
 wid_Fx = width(Tyre_Fx);
@@ -45,33 +47,41 @@ surf(alpha,Fz,Fy,'FaceAlpha',0.5); view(45,45);
 kappa_Pacejka = kappa;
 alpha_Pacejka = alpha;
 Fx_Pacejka = interp2(kappa,Fz,Fx,kappa_Pacejka,Fzf*ones(len_Fx,1));
-subplot(2,2,1); plot3(kappa_Pacejka,Fzf*ones(len_Fx,1),Fx_Pacejka,'r-','LineWidth',2);
+subplot(2,2,1); 
+plot3(kappa_Pacejka,Fzf*ones(len_Fx,1),Fx_Pacejka,'r-','LineWidth',2);
 Bxf = +425;
 Cxf = +1.650;
 Dxf = +0.700*Fzf;
 Fx_Pacejka = Dxf*sin(Cxf*atan(Bxf*kappa_Pacejka/57.3));
-subplot(2,2,1); plot3(kappa_Pacejka,Fzf*ones(len_Fx,1),Fx_Pacejka,'go:','LineWidth',2);
+subplot(2,2,1); 
+plot3(kappa_Pacejka,Fzf*ones(len_Fx,1),Fx_Pacejka,'go:','LineWidth',2);
 
 Fy_Pacejka = interp2(alpha,Fz,Fy,alpha_Pacejka,Fzf*ones(len_Fy,1));
-subplot(2,2,2); plot3(alpha_Pacejka,Fzf*ones(len_Fy,1),Fy_Pacejka,'r-','LineWidth',2);
+subplot(2,2,2); 
+plot3(alpha_Pacejka,Fzf*ones(len_Fy,1),Fy_Pacejka,'r-','LineWidth',2);
 Byf = +6.875;
 Cyf = +1.625;
 Dyf = -0.675*Fzf;
 Fy_Pacejka = Dyf*sin(Cyf*atan(Byf*alpha_Pacejka/57.3));
-subplot(2,2,2); plot3(alpha_Pacejka,Fzf*ones(len_Fy,1),Fy_Pacejka,'go:','LineWidth',2);
+subplot(2,2,2); 
+plot3(alpha_Pacejka,Fzf*ones(len_Fy,1),Fy_Pacejka,'go:','LineWidth',2);
 
 Fx_Pacejka = interp2(kappa,Fz,Fx,kappa_Pacejka,Fzr*ones(len_Fx,1));
-subplot(2,2,3); plot3(kappa_Pacejka,Fzr*ones(len_Fx,1),Fx_Pacejka,'r-','LineWidth',2);
+subplot(2,2,3); 
+plot3(kappa_Pacejka,Fzr*ones(len_Fx,1),Fx_Pacejka,'r-','LineWidth',2);
 Bxf = +475;
 Cxf = +1.600;
 Dxf = +0.735*Fzr;
 Fx_Pacejka = Dxf*sin(Cxf*atan(Bxf*kappa_Pacejka/57.3));
-subplot(2,2,3); plot3(kappa_Pacejka,Fzr*ones(len_Fx,1),Fx_Pacejka,'go:','LineWidth',2);
+subplot(2,2,3); 
+plot3(kappa_Pacejka,Fzr*ones(len_Fx,1),Fx_Pacejka,'go:','LineWidth',2);
 
 Fy_Pacejka = interp2(alpha,Fz,Fy,alpha_Pacejka,Fzr*ones(len_Fy,1));
-subplot(2,2,4); plot3(alpha_Pacejka,Fzr*ones(len_Fy,1),Fy_Pacejka,'r-','LineWidth',2);
+subplot(2,2,4); 
+plot3(alpha_Pacejka,Fzr*ones(len_Fy,1),Fy_Pacejka,'r-','LineWidth',2);
 Byf = +6.875;
 Cyf = +1.625;
 Dyf = -0.725*Fzr;
 Fy_Pacejka = Dyf*sin(Cyf*atan(Byf*alpha_Pacejka/57.3));
-subplot(2,2,4); plot3(alpha_Pacejka,Fzr*ones(len_Fy,1),Fy_Pacejka,'go:','LineWidth',2);
+subplot(2,2,4); 
+plot3(alpha_Pacejka,Fzr*ones(len_Fy,1),Fy_Pacejka,'go:','LineWidth',2);
